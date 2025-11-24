246f67f1ed22edceb4581194d9ab0e74941ae16d
Erick-Nu <erick.nunez02@epn.edu.ec>
2025-11-23 22:55:11 -0500

    [7] Enumerados

---
+++ sevenPractice.dart
@@ -0,0 +1,20 @@
+// Function main
+void main() {
+  // Enum
+  print(MyEnum.java);
+}
+
+enum MyEnum {
+  dart,
+  python,
+  java,
+  javaScript,
+  c,
+  ruby,
+  go,
+  kotlin,
+  swift,
+  rust,
+  scala,
+  typescript;
+}
