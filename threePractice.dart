fd2d273756f7bc75d6cccc9fa1382b65ffc5674b
Erick-Nu <erick.nunez02@epn.edu.ec>
2025-11-23 22:32:36 -0500

    [4] Condicionales

---
+++ threePractice.dart
@@ -0,0 +1,13 @@
+// Function main
+void main() {
+  // Conditional
+  var numOne = 4;
+  var nameNumber = "Siete";
+  if (numOne == 6 && nameNumber == "Sies") {
+    print('numOne is equal to 6 and nameNumber is equal to Sies');
+  } else if (numOne > 5 || nameNumber == "Sies") {
+    print('numOne is greater than 5 or nameNumber is equal to Sies');
+  } else {
+    print('numOne is less than 5 and nameNumber is not equal to Sies');
+  }
+}
